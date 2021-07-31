695e7ae8e2774bb516f06a38991b568c487f1137
Александр <fireostendere@gmail.com>
2021-07-31 15:40:46 +0400

    Введён класс для управления

--- fixture/application.py
+++ fixture/application.py
@@ -1,20 +1,11 @@
 from selenium import webdriver
-
+from fixture.session import SessionHelper
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -50,9 +41,6 @@ class Application:
         wd.find_element_by_link_text("groups").click()
 
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
 
 
