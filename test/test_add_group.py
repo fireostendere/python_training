9e1cfda3f2550d66f692299dd35d70e1e1b35ca5
Александр <fireostendere@gmail.com>
2021-08-01 02:43:35 +0400

    Добавление общей фикстуры

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.group import Group
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
+import time
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
     app.group.create(Group(name="test_group", header="test_group", footer="test_group"))
     app.session.logout()
+    time.sleep(1)
 
 
 def test_empty_group(app):
--- test/test_add_user.py
+++ test/test_add_user.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.user import User
-from fixture.application import Application
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
+
 
 
 def test_add_user(app):
