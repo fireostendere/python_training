2913f20b0228fcf718c93b73c1ce6d9a5b41f0b6
Александр <fireostendere@gmail.com>
2021-08-08 11:07:50 +0400

    оптимизированы переходы между страницами для всех тестов

--- fixture/application.py
+++ fixture/application.py
@@ -8,7 +8,7 @@ class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(5)
+        self.wd.implicitly_wait(0.1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.user = UserHelper(self)
@@ -22,7 +22,8 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost")
+        if not (wd.current_url.endswith("/localhost") and len(wd.find_elements_by_link_text("Last name")) > 0):
+            wd.get("http://localhost")
 
     def destroy(self):
         self.wd.quit()
