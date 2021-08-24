abe3a2f2529f079eb8a308a43db93e186f717c1f
Александр <fireostendere@gmail.com>
2021-08-25 00:55:55 +0400

    проба параметризации контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,42 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_email(maxlen, postfix):
+    symbols = string.ascii_lowercase
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))]) + "@" + postfix
+
+
+def random_number(maxlen):
+    return [random.choice(string.digits) for i in range(random.randrange(maxlen))]
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", middlename="", lastname="",
+                    homephone="", mobilephone="",
+                    workphone="", secondaryphone="",
+                    email="", email2="", email3="", address="")] + [
+               Contact(firstname=random_string("firstname", 15),
+                       middlename=random_string("middlename", 10), lastname=random_string("lastname", 5),
+                       homephone=random_number(15), mobilephone=random_number(15), workphone=random_number(15),
+                       secondaryphone=random_number(15),
+                       email=random_email(10, "mail.ru"), email2=random_email(10, "rambler.ru"),
+                       email3=random_email(10, "google.com"),
+                       address=random_string("address", 15))
+               for i in range(2)
+           ]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="test", middlename="test", lastname="test",
-                      homephone="1111", mobilephone="2222",
-                      workphone="3333", secondaryphone="4444",
-                      email="111@mail.ru", email2="222@mail.ru", email3="333@mail.ru", address="City")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
