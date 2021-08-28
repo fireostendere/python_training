4b55f639c21159646133198fa41cf5130aa89afa
Александр <fireostendere@gmail.com>
2021-08-28 16:06:40 +0400

    Реализована загрузка информации о тестируемом приложении из конфигурационного файла

--- fixture/application.py
+++ fixture/application.py
@@ -30,7 +30,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not (wd.current_url.endswith("/localhost") and len(wd.find_elements_by_link_text("Last name")) > 0):
+        if not (wd.current_url.endswith(self.base_url) and len(wd.find_elements_by_link_text("Last name")) > 0):
             wd.get(self.base_url)
 
     def destroy(self):
